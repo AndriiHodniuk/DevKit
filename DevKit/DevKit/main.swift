e5e292928e80a83b5baf85d54e4e7941279029a6
Andrii Hodniuk <andrii.hodniuk@outlook.com>
2025-08-21 11:06:41 +0200

    feat: Initial setup and welcome message

---
+++ DevKit/DevKit/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  DevKit
+//
+//  Created by Andrii Hodniuk on 21/08/2025.
+//
+
+import Foundation
+
+let appName = "DevKit"
+var version = "0.0.1"
+
+print("Welcome to \(appName) version \(version)!")
+
